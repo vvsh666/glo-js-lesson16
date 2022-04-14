415fd9dd49652cf48a2b0f78ec3fef11b2cbddab
svv <vvsh1978@mail.ru>
2022-04-14 17:09:26 +0500

    feat:make lesson16

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+class First {
+    hello() {
+        console.log('Привет я метод родителя!')
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!')
+    }
+}
+
+const firstObj = new First();
+const secondObj = new Second();
+
+firstObj.hello()
+secondObj.hello()
